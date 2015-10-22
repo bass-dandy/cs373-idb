7e3d45585fff458357645331d7d3c8ba205ed0f9
KaelenHaag1 <khaag@utexas.edu>
2015-10-22 11:36:57 -0500

    Some Test cases for Song Model #16

--- tests.py
+++ tests.py
@@ -35,8 +35,35 @@ class MusicMeccaModelsTests(TestCase):
         assertEquals(artist.name == "Kendrick Lamar")
 
     def test_artist_not_available(self):
-        artist = Artist.query().filter_by(name="Katy Perry")
+        artist = Artist.query().filter_by(name="Katy Perry").first()
         assertIsNone(artist)
+
+    #---------------
+    # Song Tests
+    #---------------
+
+    def test_song_by_id(self):
+        db.session.add(Artist(name="Taylor Swift"))
+        db.session.add(Song(name="Bad Blood",lyrics="Lyrics go here", artist=[00001, 00002]))
+        db.session.commit()
+
+        song = Song.query.get(00001)
+        assertEquals(song.name, "Bad Blood")
+        assertEquals(song.lyrics, "Lyrics go here")
+        artist = Artist.query.get(song.artist[0])
+        assertEquals(artist.name, "Taylor Swift")
+
+    def test_song_by_name(self):
+        db.session.add(Artist(name="Kendrick Lamar"))
+        db.session.add(Song(name="m.A.A.d City", lyrics="Lyrics", artist=[00002]))
+        db.session.commit()
+
+        song = Song.query.filter_by(name="m.A.A.D City").first()
+        assertEquals(song.name, "m.A.A.D City")
+        assertEquals(song.lyrics, "Lyrics")
+        artist = Artist.query.get(song.artist[0])
+        assertEquals(artist.name, "Kendrick Lamar")
+        
 
 
 
