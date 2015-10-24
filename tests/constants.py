7f5db61edd9437fadf3f917c0b6b3a5c8718b341
rioscesar <rios_cesar@utexas.edu>
2015-10-24 16:43:14 -0500

    constants file

---
+++ tests/constants.py
@@ -0,0 +1,20 @@
+### Test Artists ###
+EMINEM = 1
+
+### Test Awards ###
+BEST_IN_THE_WORLD = 1
+
+### Tests Concerts ###
+ACL = 1
+
+### Test Labels ###
+SLIM_SHADY_RECORDS = 1
+
+### Test Releases ###
+RECOVERY = 3
+
+### Test Songs ###
+FACK = 1
+
+### Test Videos ###
+FACK_VIDEO = 1
