8aa22153a49d5a0640c883d152552926eacfcecb
Christian Dinh <ctdinh93@gmail.com>
2015-11-01 19:05:27 -0600

    Remove all redundant markup and temporary images, fill all pages using json objects, make page transitions pretty, fully implement SPA behavior

--- static/js/routing-main.js
+++ static/js/routing-main.js
@@ -1,4 +1,4 @@
-var app = angular.module('meccaApp', ['ngRoute']);
+var app = angular.module('meccaApp', ['ngRoute', 'ngAnimate']);
 
 app.controller('mainController', function($scope) {
     
@@ -38,4 +38,34 @@ app.config(function($routeProvider, $locationProvider) {
         });
 
     $locationProvider.html5Mode(true);
+});
+
+app.controller('artistController', function($scope, $http, $routeParams) {
+    $http.get("tmp/" + $routeParams.id + ".json")
+        .then(function(response) {
+            $scope.artist = response.data;
+            console.log(response.data);
+        }, function() {
+            console.log("Failed to retrieve artist data");
+        });
+});
+
+app.controller('labelController', function($scope, $http, $routeParams) {
+    $http.get("tmp/" + $routeParams.id + ".json")
+        .then(function(response) {
+            $scope.label = response.data;
+            console.log(response.data);
+        }, function() {
+            console.log("Failed to retrieve label data");
+        });
+});
+
+app.controller('releaseController', function($scope, $http, $routeParams) {
+    $http.get("tmp/" + $routeParams.id + ".json")
+        .then(function(response) {
+            $scope.release = response.data;
+            console.log(response.data);
+        }, function() {
+            console.log("Failed to retrieve release data");
+        });
 });
