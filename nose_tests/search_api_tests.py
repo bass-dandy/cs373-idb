639bafb47510adaad587be04134673692b88085a
Christian Dinh <ctdinh93@gmail.com>
2015-11-18 19:57:03 -0600

    Cesar told me to

--- nose_tests/search_api_tests.py
+++ nose_tests/search_api_tests.py
@@ -6,13 +6,13 @@ class SearchAPITest(NDTestCase):
 
     def setUp(self):
         super(SearchAPITest, self).setUp()
-    #
-    # def test_get(self):
-    #     response = self.json_get('{}/?q=Eminem'.format(app.config['BASE_URL']))
-    #
-    #     reponse_dict = self.dict_from_response(response)
-    #
-    #     self.assertEqual(response.status_code, app.config['OK'])
+        
+    def test_get(self):
+        response = self.json_get('{}/?q=Eminem'.format(app.config['BASE_URL']))
+    
+        reponse_dict = self.dict_from_response(response)
+    
+        self.assertEqual(response.status_code, app.config['OK'])
 
     def test_get(self):
         response = self.json_get('{}/?q=Em'.format(app.config['BASE_URL']))
