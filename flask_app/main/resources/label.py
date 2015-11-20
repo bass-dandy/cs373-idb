2046c5061c9f4767696cb154ddf1da893a5ffe36
rioscesar <rios_cesar@utexas.edu>
2015-11-19 21:50:18 -0600

    closes #83 server side pagination is a GO

--- flask_app/main/resources/label.py
+++ flask_app/main/resources/label.py
@@ -1,23 +1,12 @@
+from flask.ext.restful import reqparse
 from flask_restful import Resource, abort
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import DataError
-from sqlalchemy import asc
+from sqlalchemy import asc, desc
 from flask_app.main.models import Label
 from flask_app import app
 from flask_app.main.resources.schemas.artist import LabelSchema
 
-
-class LabelAllAPI(Resource):
-    """All labels in Label table"""
-
-    def get(self):
-        """get all labels"""
-        try:
-            labels = Label.query.order_by(asc(Label.name)).all()
-        except (DataError, NoResultFound):
-            abort(app.config['NOT_FOUND'], message=app.config['LABEL_NOT_FOUND'].format(id))
-
-        return LabelSchema(many=True).dump(labels).data
 
 class LabelIDAPI(Resource):
     """Single label through id"""
@@ -45,3 +34,31 @@ class LabelNameAPI(Resource):
             abort(app.config['NOT_FOUND'], message=app.config['ARTIST_NOT_FOUND'].format(actualLabelName))
 
         return LabelSchema().dump(labels, many=True).data
+
+
+class LabelAllAPI(Resource):
+    """All label in Label table"""
+
+    def get(self):
+        """get all label"""
+        parser = reqparse.RequestParser()
+        parser.add_argument('page', type=int)
+        parser.add_argument('order', type=str)
+        parser.add_argument('pagesize', type=int)
+        args = parser.parse_args()
+
+        try:
+            sort = asc
+            if 'order' in args and args['order'] is not None:
+                sort = asc if args['order'] == 'asc' else desc
+            if 'page' in args and 'pagesize' in args and args['page'] is not None and args['pagesize'] is not None:
+                try:
+                    label = Label.query.order_by(sort(Label.name)).paginate(args['page'], args['pagesize']).items
+                except Exception:
+                    return {}
+            else:
+                label = Label.query.order_by(sort(Label.name)).all()
+        except (DataError, NoResultFound):
+            abort(app.config['NOT_FOUND'], message=app.config['RELEASE_NOT_FOUND'].format(id))
+
+        return LabelSchema().dump(label, many=True).data
--- nose_tests/artist_api_tests.py
+++ nose_tests/artist_api_tests.py
@@ -15,3 +15,38 @@ class ArtistAPITest(NDTestCase):
         reponse_dict = self.dict_from_response(response)
 
         self.assertEqual(response.status_code, app.config['OK'])
+
+    def test_get_by_name(self):
+        get_uri = '{}/artists/{}'.format(app.config['BASE_URL'], 'Eminem')
+        response = self.json_get(get_uri)
+
+        reponse_dict = self.dict_from_response(response)
+
+        self.assertEqual(response.status_code, app.config['OK'])
+
+    def test_get_all(self):
+        get_uri = '{}/artists/'.format(app.config['BASE_URL'])
+        response = self.json_get(get_uri)
+
+        reponse_dict = self.dict_from_response(response)
+
+        self.assertEqual(response.status_code, app.config['OK'])
+
+        get_uri = '{}/artists'.format(app.config['BASE_URL'])
+        response = self.json_get(get_uri)
+
+        reponse_dict_cmp = self.dict_from_response(response)
+
+        self.assertEqual(response.status_code, app.config['OK'])
+
+        self.assertEqual(reponse_dict, reponse_dict_cmp)
+
+    def test_get_all_pagination(self):
+        get_uri = '{}/artists?page=1&order=asc&pagesize=12'.format(app.config['BASE_URL'])
+        response = self.json_get(get_uri)
+
+        reponse_dict = self.dict_from_response(response)
+
+        self.assertEqual(response.status_code, app.config['OK'])
+
+        self.assertEqual(reponse_dict[0]['bio'], 'Rapper')
